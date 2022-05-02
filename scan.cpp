fe76113efa5320b3b9ce821517026cffcf126e5e
eugene <eteresh@gmail.com>
2022-05-02 21:57:38 +0300

    rename parameters to beta, x0

--- scan.cpp
+++ scan.cpp
@@ -10,8 +10,8 @@
 using std::cout, std::endl, std::ifstream, std::ofstream, std::string, arma::vec;
 
 
-size_t LORENZ_WIDTH = 500; // points
-size_t N_WIDTHS = 20;      // widths in full scan
+size_t LORENZ_WIDTH = 450; // points
+size_t N_WIDTHS = 10;      // widths in full scan
 
 
 vec smooth(vec temperature, int n_smooth_points = 300) {
@@ -28,72 +28,76 @@ vec smooth(vec temperature, int n_smooth_points = 300) {
 }
 
 
-vec get_power(vec& temperature, double alpha) {
+vec get_power(vec& temperature, double beta) {
   double center = temperature.n_elem / 2;
   vec power(temperature.n_elem, arma::fill::zeros);
   for (size_t i = 0; i < temperature.n_elem; i++) {
-    double offset = (-alpha * temperature[i] + (static_cast<double>(i) - center) / static_cast<double>(LORENZ_WIDTH));
+    double offset = (-beta * temperature[i] + (static_cast<double>(i) - center) / static_cast<double>(LORENZ_WIDTH));
     power[i] = 1.0 / (1.0 + offset * offset);
   }
   return power;
 }
 
 
-vec transform(vec& x, double beta) {
+vec transform(vec& x, double x0) {
   double exponential_average = 0.0;
-  double damping_factor = std::exp(-beta);
+  double damping_factor = std::exp(-1.0 / x0);
   vec result(x.n_elem, arma::fill::zeros);
   for (size_t i = 0; i < x.n_elem; i++) {
     exponential_average = x[i] + damping_factor * exponential_average;
-    result[i] = beta * exponential_average;
+    result[i] = exponential_average / x0;
   }
   return result;
 }
 
 
-void parse_args(int argc, char **argv, double& alpha, double& beta, size_t& simulation_iterations) {
+void parse_args(int argc, char **argv, double& beta, double& x0, size_t& simulation_iterations) {
   if (argc >= 2) {
-    std::stringstream alpha_stream(argv[1]);
-    alpha_stream >> alpha;
+    std::stringstream beta_stream(argv[1]);
+    beta_stream >> beta;
   }
 
   if (argc >= 3) {
-    std::stringstream beta_stream(argv[2]);
-    beta_stream >> beta;
+    std::stringstream x0_stream(argv[2]);
+    x0_stream >> x0;
   }
 
   if (argc >= 4) {
     std::stringstream iters_stream(argv[3]);
     iters_stream >> simulation_iterations;
   }
-  cout << "alpha (temperature contribution): " << alpha << endl;
-  cout << "beta (inverse scan speed): " << beta << endl;
+  cout << "beta (temperature contribution): " << beta << endl;
+  cout << "x0 (scan speed): " << x0 << endl;
   cout << "simulation iterations: " << simulation_iterations << endl;
 }
 
 
 int main(int argc, char **argv) {
   size_t n_points = 2 * LORENZ_WIDTH * N_WIDTHS;
-  double alpha = 1.0;
-  double beta = 0.05;
+  double beta = 1.0;
+  double x0 = 20.0;
   size_t simulation_iterations = 100000;
-  parse_args(argc, argv, alpha, beta, simulation_iterations);
+  parse_args(argc, argv, beta, x0, simulation_iterations);
 
   vec temperature(n_points, arma::fill::zeros);
-  vec power = get_power(temperature, alpha);
+  vec power = get_power(temperature, beta);
 
   tqdm bar;
   for (size_t j = 0; j < simulation_iterations; j++) {
-    temperature = transform(power, beta);
+    temperature = transform(power, x0);
     // temperature = smooth(temperature, n_smooth_points);
-    power = get_power(temperature, alpha);
+    power = get_power(temperature, beta);
     bar.progress(j + 1, simulation_iterations);
+  }
+  if (beta < 0.0) {
+    temperature = arma::reverse(temperature);
+    power = arma::reverse(power);
   }
   cout << endl;
 
   // saving simulation results
   std::stringstream hdf5_filename;
-  hdf5_filename << "data/" << "alpha_" << alpha << "_beta_" << beta << "_simulation_iterations_" << simulation_iterations << ".hdf5";
+  hdf5_filename << "data/" << "beta_" << beta << "_x0_" << x0 << "_simulation_iterations_" << simulation_iterations << ".hdf5";
   temperature.save(arma::hdf5_name(hdf5_filename.str(), "temperature"), arma::hdf5_binary);
   power.save(arma::hdf5_name(hdf5_filename.str(), "power", arma::hdf5_opts::append), arma::hdf5_binary);
   cout << "temperature and power arrays saved to: " << endl << hdf5_filename.str() << endl;
